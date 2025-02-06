cdb69efe901f76f3e8461807cecd0b8a1ffbbc27
BlueShadow <bluemodsyt@gmail.com>
2025-02-07 05:45:54 +0800

    Create main.js

---
+++ glitch-b/scripts/main.js
@@ -0,0 +1,10 @@
+import "./systems/bowding.js";
+import "./systems/cps.js";
+import "./systems/cranks.js";
+import "./systems/dailyrewards.js";
+import "./systems/emoji.js";
+import "./systems/index.js";
+import "./systems/kill.js";
+import "./systems/scoreboard.js";
+import "./systems/sellpad.js";
+import "./systems/starterkit.js";
