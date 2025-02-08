1aff5390d4abc446d8f5dff2dfe9c6e538df194a
BlueShadow <bluemodsyt@gmail.com>
2025-02-09 07:32:16 +0800

    Create test.js

---
+++ glitch-b/scripts/commands/public/test.js
@@ -0,0 +1,13 @@
+import { world, system } from "@minecraft/server";
+import { Command } from "../CommandHandler.js";
+import main from "../config.js";
+
+Command.register({
+    name: "test",
+    description: "dummy command",
+    aliases: [],   
+    // <- (remove if the command is for admin) | permission: (player) => player.hasTag(main.adminTag),
+}, async (data) => {
+    player.sendMessage(`Successfully Executed`);
+    player.runCommandAsync(`playsound random.levelup`);
+});
